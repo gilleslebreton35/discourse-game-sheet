e407d310fa6a72d2a69a8c16f53b9c94057c78c3
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-24 12:11:37 +0200

    Update game_sheet_controller.rb

--- app/controllers/discourse_game_sheet/game_sheet_controller.rb
+++ app/controllers/discourse_game_sheet/game_sheet_controller.rb
@@ -8,47 +8,47 @@ module DiscourseGameSheet
     def search
       params.require(:q)
       
-      # TODO: Remplacer par ton vrai appel BGG pour la recherche
-      # Exemple : results = BggApiService.search(params[:q])
+      # On appelle ton client BGG
+      data = DiscourseGameSheet::BggClient.search(params[:q])
       
-      # En attendant que tu branches BGG, on renvoie un faux résultat pour valider que le lien passe bien :
-      render json: { success: true, message: "La route search fonctionne !", query: params[:q] }
+      # Ton client renvoie { results: [...] }, on le transmet directement au Javascript
+      render json: data
     end
 
     # GET /game-sheet/details?id=123
     def details
       params.require(:id)
       
-      # Logique d'interrogation de l'API BGG Thing v2
-      game_details = BggApiService.fetch_thing_details(params[:id])
-      
-      if game_details
+      begin
+        # Utilisation de la méthode "game" de ton client
+        game_details = DiscourseGameSheet::BggClient.game(params[:id])
         render json: game_details 
-      else
-        render_json_error "Impossible d'extraire les données depuis BoardGameGeek.", status: 422
+      rescue StandardError => e
+        render_json_error "Erreur BGG : #{e.message}", status: 422
       end
     end
 
     # POST /game-sheet/create-topic
     def create_topic
-      # Validation stricte des données entrantes (Strong Parameters)
       clean_params = params.permit(:game_id, :category_id, selected_images: [])
       
       clean_params.require(:game_id)
       clean_params.require(:category_id)
 
-      # Récupération des détails (idéalement mis en cache ou relus)
-      game = BggApiService.fetch_thing_details(clean_params[:game_id])
-      return render_json_error "Jeu introuvable.", status: 404 if game.blank?
+      begin
+        # Utilisation de la méthode "game" de ton client
+        game = DiscourseGameSheet::BggClient.game(clean_params[:game_id])
+      rescue StandardError
+        return render_json_error "Jeu introuvable sur BoardGameGeek.", status: 404
+      end
 
       # Construction du corps Markdown
       raw_body = String.new
       
-      # 1. Injection des illustrations sélectionnées en tête d'article
+      # 1. Injection des illustrations
       if clean_params[:selected_images].present?
         raw_body << "<div class='bgg-topic-gallery'>\n\n"
         clean_params[:selected_images].each do |img_url|
-          # Correction de la syntaxe d'interpolation Ruby : #{variable}
           raw_body << "![Illustration](#{img_url})\n\n"
         end
         raw_body << "</div>\n\n"
@@ -56,15 +56,15 @@ module DiscourseGameSheet
 
       # 2. Injection de la fiche technique
       raw_body << "## #{game[:name]}\n\n"
-      raw_body << "* **Note BGG :** #{game[:rating]}/10\n"
-      raw_body << "* **Joueurs :** #{game[:min_players]} à #{game[:max_players]}\n"
-      raw_body << "* **Durée :** #{game[:playing_time]} minutes\n\n"
+      raw_body << "* **Année :** #{game[:yearpublished]}\n"
+      raw_body << "* **Joueurs :** #{game[:minplayers]} à #{game[:maxplayers]}\n"
+      raw_body << "* **Durée :** #{game[:playingtime]} minutes\n"
+      raw_body << "* **Âge minimum :** #{game[:minage]} ans\n\n"
       raw_body << "### Description\n\n"
       
-      # Utilisation du helper Discourse pour nettoyer le HTML de BGG avant insertion
       raw_body << Search.clean_html(game[:description])
 
-      # 3. Appel à l'orchestrateur natif Discourse PostCreator
+      # 3. Création du Post
       post_creator = PostCreator.new(
         current_user,
         title: "Fiche de jeu : #{game[:name]}",
