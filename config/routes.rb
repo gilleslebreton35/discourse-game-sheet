1dbac3038d1222b5681ab9edb1d7fb8febafd246
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-24 09:51:06 +0200

    Delete config/routes.rb

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-DiscourseGameSheet::Engine.routes.draw do
-  get "/search" => "game_sheet#search"
-  get "/details" => "game_sheet#details"
-  post "/create-topic" => "game_sheet#create_topic"
-end
-
-Discourse::Application.routes.append do
-  mount ::DiscourseGameSheet::Engine, at: "/game-sheet"
-end
