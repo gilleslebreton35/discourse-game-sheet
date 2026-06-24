47f45e264a85cc687e22e25acad6f853fb5d4c1a
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-24 09:05:10 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-# frozen_string_literal: true
-
 DiscourseGameSheet::Engine.routes.draw do
   get "/search" => "game_sheet#search"
-  get "/game/:id" => "game_sheet#game"
+  get "/details" => "game_sheet#details"
   post "/create-topic" => "game_sheet#create_topic"
+end
+
+Discourse::Application.routes.append do
+  mount ::DiscourseGameSheet::Engine, at: "/game-sheet"
 end
