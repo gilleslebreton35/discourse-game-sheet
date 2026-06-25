8b8eeb69a93f00e412c489a119a181f346b7f095
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-25 10:54:27 +0200

    Update deepl_client.rb

--- app/services/discourse_game_sheet/deepl_client.rb
+++ app/services/discourse_game_sheet/deepl_client.rb
@@ -1,55 +1,30 @@
 # frozen_string_literal: true
-
 require "net/http"
-require "uri"
 require "json"
 
 module ::DiscourseGameSheet
   class DeeplClient
-    API_URL = "https://api-free.deepl.com/v2/translate"
+    def self.translate(text)
+      api_key = SiteSetting.game_sheet_deepl_api_key.to_s.strip
+      return text if api_key.blank? || text.blank?
 
-    def self.translate_game(game, api_key:, target_lang:)
-      translated = game.deep_dup
+      # Détection automatique de l'API gratuite ou payante de DeepL via le suffixe :fx
+      url = api_key.end_with?(":fx") ? "https://api-free.deepl.com/v2/translate" : "https://api.deepl.com/v2/translate"
+      uri = URI(url)
 
-      translated["description"] = translate_text(
-        game[:description] || game["description"],
-        api_key: api_key,
-        target_lang: target_lang
-      )
-
-      translated["categories"] = Array(game[:categories] || game["categories"]).map do |c|
-        translate_text(c, api_key: api_key, target_lang: target_lang)
-      end
-
-      translated["mechanics"] = Array(game[:mechanics] || game["mechanics"]).map do |m|
-        translate_text(m, api_key: api_key, target_lang: target_lang)
-      end
-
-      translated
-    end
-
-    def self.translate_text(text, api_key:, target_lang:)
-      return text if text.blank?
-
-      uri = URI(API_URL)
-      req = Net::HTTP::Post.new(uri)
-      req["Authorization"] = "DeepL-Auth-Key #{api_key}"
-
-      req.set_form_data({
+      response = Net::HTTP.post_form(uri, {
         "text" => text,
-        "target_lang" => target_lang
+        "target_lang" => "FR",
+        "auth_key" => api_key
       })
 
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      response = http.request(req)
-
-      unless response.is_a?(Net::HTTPSuccess)
-        raise "DeepL request failed: #{response.code}"
+      if response.is_a?(Net::HTTPSuccess)
+        json = JSON.parse(response.body)
+        json.dig("translations", 0, "text") || text
+      else
+        Rails.logger.error("[Game Sheet] Échec DeepL : #{response.body}")
+        text
       end
-
-      parsed = JSON.parse(response.body)
-      parsed.dig("translations", 0, "text") || text
     end
   end
 end
