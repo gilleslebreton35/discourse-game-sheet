d390a6618fc054a093513b82f234be36a3594bd7
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-25 19:15:21 +0200

    Create game-sheet.js

---
+++ assets/javascripts/discourse/routes/game-sheet.js
@@ -0,0 +1,5 @@
+import DiscourseRoute from "discourse/routes/discourse";
+
+export default class GameSheetRoute extends DiscourseRoute {
+  // Optionnel : on peut configurer le titre de la page ou d'autres comportements ici plus tard
+}
