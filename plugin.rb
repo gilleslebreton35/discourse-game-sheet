8034ef20c7bc35a59dd86f79f5880aed63401574
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-25 10:53:43 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,33 +1,33 @@
-# frozen_string_literal: true
-
 # name: discourse-game-sheet
-# about: Create board game topics from BGG + DeepL
-# version: 0.3
-# authors: Gilles
-# url: https://github.com/gilleslebreton35/discourse-game-sheet
+# about: Crée des fiches de jeux de société via BGG et DeepL
+# version: 2.0.0
+# authors: Ton Nom
+# url: https://github.com/ton-pseudo/discourse-game-sheet
 
 enabled_site_setting :game_sheet_enabled
 
-register_asset "stylesheets/common/game-sheet.scss"
+register_asset "stylesheets/game-sheet.scss" # Si tu veux ajouter du CSS plus tard
 
 after_initialize do
-  require_relative "app/services/discourse_game_sheet/bgg_client"
-  require_relative "app/services/discourse_game_sheet/deepl_client"
-  require_relative "app/services/discourse_game_sheet/topic_builder"
-  require_relative "app/controllers/discourse_game_sheet/game_sheet_controller"
-
-  Discourse::Application.routes.prepend do
-    # Route pour la page principale (accessible aux membres connectés)
-    get "/game-sheet" => "discourse_game_sheet/game_sheet#index"
-    
-    # Routes API (accessibles aux membres connectés)
-    get "/game-sheet/search" => "discourse_game_sheet/game_sheet#search", defaults: { format: :json }
-    get "/game-sheet/game/:id" => "discourse_game_sheet/game_sheet#game", defaults: { format: :json }
-    post "/game-sheet/create-topic" => "discourse_game_sheet/game_sheet#create_topic", defaults: { format: :json }
+  module ::DiscourseGameSheet
+    class Engine < ::Rails::Engine
+      engine_name "discourse_game_sheet"
+      isolate_namespace DiscourseGameSheet
+    end
   end
 
-  # Route admin qui redirige vers la page publique
+  # Chargement des fichiers manuellement pour s'assurer de leur présence
+  load File.expand_path("../app/services/discourse_game_sheet/bgg_client.rb", __FILE__)
+  load File.expand_path("../app/services/discourse_game_sheet/deepl_client.rb", __FILE__)
+  load File.expand_path("../app/controllers/discourse_game_sheet/game_sheet_controller.rb", __FILE__)
+
+  DiscourseGameSheet::Engine.routes.draw do
+    get "/search" => "game_sheet#search"
+    get "/details" => "game_sheet#details"
+    post "/create-topic" => "game_sheet#create_topic"
+  end
+
   Discourse::Application.routes.append do
-    get "/admin/plugins/game-sheet" => redirect("/game-sheet"), constraints: AdminConstraint.new
+    mount ::DiscourseGameSheet::Engine, at: "/game-sheet"
   end
 end
