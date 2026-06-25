3801aa35d01db03621ab546d62cc8e6c2274b4cb
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-25 10:55:45 +0200

    Delete assets/javascripts/discourse-game-sheet-admin-route-map.js

--- assets/javascripts/discourse-game-sheet-admin-route-map.js
+++
@@ -1,9 +0,0 @@
-export default {
-  model() {
-    return this.api.adminPlugin("discourse-game-sheet");
-  },
-
-  setupController(controller, model) {
-    controller.set("model", model);
-  },
-};
