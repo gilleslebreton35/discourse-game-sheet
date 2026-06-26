922485725ca3c0255260130226205039b9c84a87
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-26 13:10:42 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: discourse-game-sheet
-# about: Plugin pour créer des fiches de jeux avec sélection de médias
+# about: Plugin pour créer des fiches de jeux
 # version: 0.6
 
 enabled_site_setting :game_sheet_enabled
@@ -11,13 +11,9 @@ after_initialize do
   require 'net/http'
   require 'uri'
 
-  # Définition du paramètre admin pour filtrer les catégories
-  register_editable_site_setting :game_sheet_allowed_category_ids, default: "1|2|3"
-
   module DiscourseGameSheet
     class BggClient
       BGG_TOKEN = "a904f3bf-f154-4890-9618-4dc3835e40c7"
-
       def self.request_bgg(path)
         uri = URI.parse("https://boardgamegeek.com/xmlapi2/#{path}")
         http = Net::HTTP.new(uri.host, uri.port)
@@ -30,14 +26,12 @@ after_initialize do
       def self.search(query)
         resp = request_bgg("search?query=#{ERB::Util.url_encode(query)}&type=boardgame")
         return { bgg: [] } unless resp.is_a?(Net::HTTPSuccess)
-        
         doc = Nokogiri::XML(resp.body)
         ids = doc.xpath('//item').map { |i| i['id'] }.first(5)
         return { bgg: [] } if ids.empty?
 
         resp_details = request_bgg("thing?id=#{ids.join(',')}")
         doc_details = Nokogiri::XML(resp_details.body)
-        
         results = doc_details.xpath('//item').map do |item|
           { id: item['id'], name: item.at_xpath('name')&.[]('value'), image: item.at_xpath('thumbnail')&.text }
         end
@@ -48,7 +42,6 @@ after_initialize do
         resp = request_bgg("thing?id=#{id}&stats=1&videos=1")
         doc = Nokogiri::XML(resp.body)
         item = doc.at_xpath('//item')
-
         {
           id: id,
           name: item.at_xpath('name')&.[]('value'),
@@ -72,6 +65,7 @@ after_initialize do
     end
 
     def categories
+      # Accès direct via SiteSetting
       allowed_ids = SiteSetting.game_sheet_allowed_category_ids.split('|').map(&:to_i)
       render json: Category.where(id: allowed_ids).map { |c| { id: c.id, name: c.name } }
     end
@@ -80,7 +74,6 @@ after_initialize do
       data = params.permit(:game_id, :category_id, images: [], videos: [:link, :title])
       game = DiscourseGameSheet::BggClient.game_details(data[:game_id])
       
-      # Construction du Markdown
       raw = ""
       data[:images]&.each { |img| raw << "![image|600](#{img})\n\n" }
       data[:videos]&.each { |vid| raw << "[Vidéo : #{vid[:title]}](#{vid[:link]})\n\n" }
