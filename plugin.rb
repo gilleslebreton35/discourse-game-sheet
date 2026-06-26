729d1e2b9656fd4f433b7b4968e1d21f4f815363
gilleslebreton35 <lebretongilles35@sfr.fr>
2026-06-26 13:33:36 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,7 @@
 # name: discourse-game-sheet
-# about: Plugin pour créer des fiches de jeux
+# about: Plugin pour créer des fiches de jeux depuis BGG
 # version: 0.6
+# authors: Toi
 
 enabled_site_setting :game_sheet_enabled
 
@@ -10,7 +11,8 @@ after_initialize do
   require 'net/http'
   require 'uri'
 
-  module DiscourseGameSheet
+  # 1. Définition du module pour isoler la logique
+  module ::DiscourseGameSheet
     class BggClient
       BGG_TOKEN = "a904f3bf-f154-4890-9618-4dc3835e40c7"
       BASE_URL = "https://boardgamegeek.com/xmlapi2"
@@ -24,47 +26,55 @@ after_initialize do
         http.request(request)
       end
 
-      # ... (garde ta méthode self.search comme elle est dans ta v0.5) ...
       def self.search(query)
         return { bgg: [] } if query.blank?
         encoded_query = ERB::Util.url_encode(query.to_s.strip)
         resp = request_bgg("search?query=#{encoded_query}&type=boardgame")
         return { bgg: [] } unless resp.is_a?(Net::HTTPSuccess)
+        
         doc = Nokogiri::XML(resp.body)
         ids = doc.xpath('//item').map { |i| i['id'] }.first(10)
         return { bgg: [] } if ids.empty?
+
         resp_details = request_bgg("thing?id=#{ids.join(',')}")
         return { bgg: [] } unless resp_details.is_a?(Net::HTTPSuccess)
+        
         details_doc = Nokogiri::XML(resp_details.body)
         results = details_doc.xpath('//item').map do |item|
-          { id: item['id'], name: item.at_xpath('name')&.[]('value'), image: item.at_xpath('thumbnail')&.text }
+          {
+            id: item['id'],
+            name: item.at_xpath('name')&.[]('value'),
+            yearpublished: item.at_xpath('yearpublished')&.[]('value'),
+            image: item.at_xpath('thumbnail')&.text
+          }
         end
         { bgg: results }
       end
 
       def self.game_details(id)
-        resp = request_bgg("thing?id=#{id}&stats=1&videos=1")
+        resp = request_bgg("thing?id=#{id}&stats=1")
         return { error: "Non trouvé" } unless resp.is_a?(Net::HTTPSuccess)
         doc = Nokogiri::XML(resp.body)
         item = doc.at_xpath('//item')
+        return { error: "Non trouvé" } unless item
         {
           id: id,
           name: item.at_xpath('name')&.[]('value'),
           description: item.at_xpath('description')&.text&.gsub(/&amp;/, '&'),
-          images: doc.xpath('//image').map(&:text).first(5), # Récupère 5 images
-          videos: doc.xpath('//video').select { |v| v['language'] == 'French' }.map { |v| { link: v['link'], title: v['title'] } }
+          image: item.at_xpath('image')&.text
         }
       end
     end
   end
 
+  # 2. Définition du contrôleur (avec le scope global ::)
   class ::GameSheetController < ::ApplicationController
+    requires_plugin "discourse-game-sheet"
     before_action :ensure_logged_in
-    
-    def categories
-      # Utilise le réglage défini dans settings.yml
-      allowed_ids = SiteSetting.game_sheet_allowed_category_ids.split('|').map(&:to_i)
-      render json: Category.where(id: allowed_ids).map { |c| { id: c.id, name: c.name } }
+    skip_before_action :check_xhr, only: [:index]
+
+    def index
+      render html: "", layout: true
     end
 
     def search
@@ -75,22 +85,27 @@ after_initialize do
       render json: DiscourseGameSheet::BggClient.game_details(params[:id])
     end
 
+    def categories
+      # Utilise le SiteSetting défini dans le YML
+      allowed_ids = SiteSetting.game_sheet_allowed_category_ids.split('|').map(&:to_i)
+      render json: Category.where(id: allowed_ids).map { |c| { id: c.id, name: c.name } }
+    end
+
     def create_topic
-      data = params.permit(:game_id, :category_id, images: [], videos: [:link, :title])
-      game = DiscourseGameSheet::BggClient.game_details(data[:game_id])
+      game = DiscourseGameSheet::BggClient.game_details(params[:game_id])
+      return render json: { error: game[:error] }, status: 400 if game[:error]
       
-      # Construction du Markdown avec les éléments sélectionnés
-      raw = ""
-      data[:images]&.each { |img| raw << "![image|600](#{img})\n\n" }
-      data[:videos]&.each { |vid| raw << "[Vidéo : #{vid[:title]}](#{vid[:link]})\n\n" }
-      raw << "### Description\n#{game[:description]}"
+      image_markdown = game[:image].present? ? "![image|600](#{game[:image]})\n\n" : ""
+      raw = "#{image_markdown}### Description\n#{game[:description]}"
       
-      post = PostCreator.new(current_user, title: "Fiche : #{game[:name]}", raw: raw, category: data[:category_id]).create
+      post = PostCreator.new(current_user, title: "Fiche : #{game[:name]}", raw: raw, category: params[:category_id]).create
       post&.persisted? ? render(json: { topic_url: post.topic.url }) : render(json: { error: "Erreur" }, status: 422)
     end
   end
 
+  # 3. Enregistrement des routes
   Discourse::Application.routes.append do
+    get "/game-sheet" => "game_sheet#index"
     get "/game-sheet-api/search" => "game_sheet#search"
     get "/game-sheet-api/details/:id" => "game_sheet#details"
     get "/game-sheet-api/categories" => "game_sheet#categories"
